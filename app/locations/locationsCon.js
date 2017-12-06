5f004c3ef711579ac9ef5acc38e264838b053ed2
Jnixon54 <Jnixon54@gmail.com>
2017-12-06 14:46:59 -0600

    final

--- app/locations/locationsCon.js
+++ app/locations/locationsCon.js
@@ -1,5 +1,8 @@
-angular.module('devmtnTravel').controller('locationsCon', function($scope, mainSrvc){
-  mainSrvc
-  .getTravelInfo()
-  .then(response => {console.log(response); $scope.locations = response})
-});
+angular
+  .module("devmtnTravel")
+  .controller("locationsCon", function($scope, mainSrvc) {
+    mainSrvc.getTravelInfo().then(response => {
+      console.log(response);
+      $scope.locations = response;
+    });
+  });
